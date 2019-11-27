2acf5d7c35098cf7284f44cc4c9e54464a024d5a
raja448 <56379969+raja448@users.noreply.github.com>
2019-11-27 14:36:51 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,3 @@
-variable "key_name" {
-  default = "rajasekkar"
-}
-
-variable "pvt_key" {
-  default = "/home/rajasekkar.pem"
-}
-
 variable "us-east-zones" {
   default = ["us-east-1a", "us-east-1b"]
 }
