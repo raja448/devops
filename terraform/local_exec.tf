71c242975de4ea68fe1e129f2740cf3724795be5
raja448 <56379969+raja448@users.noreply.github.com>
2019-12-01 16:38:10 +0530

    Update local_exec.tf

--- terraform/local_exec.tf
+++ terraform/local_exec.tf
@@ -13,8 +13,9 @@ provider "aws" {
 resource "aws_instance" "backend" {
   ami                    = "ami-04763b3055de4860b"
   instance_type          = "t2.micro"
-  key_name               = "${var.key_name}" 
-  vpc_security_group_ids = ["${var.sg_id}"]
+  key_name               = "${var.key_name}"
+  vpc_security_group_ids = ["${var.sg-id}"]
+  
   }
 
 resource "null_resource" "remote-exec-1" {
