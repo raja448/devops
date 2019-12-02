54f2a8e7de5ce9cbf1fb241f3b68328d5f0a7cc6
raja448 <56379969+raja448@users.noreply.github.com>
2019-12-02 11:01:48 +0530

    Update local_exec.tf

--- terraform/local_exec.tf
+++ terraform/local_exec.tf
@@ -2,26 +2,27 @@ terraform {
   backend "local" {
     path = "/tmp/terraform/workspace/terraform.tfstate"
   }
-  }
+
+}
 
 provider "aws" {
-    region = "us-east-1"
-
-  }
-
+  region = "us-east-1"
+  
+}
 
 resource "aws_instance" "backend" {
   ami                    = "ami-04763b3055de4860b"
   instance_type          = "t2.micro"
   key_name               = "${var.key_name}"
   vpc_security_group_ids = ["${var.sg-id}"]
-  
-  }
+
+}
 
 resource "null_resource" "remote-exec-1" {
     connection {
-    user        = "root"
+    user        = "ubuntu"
     type        = "ssh"
+    private_key = "${file(var.pvt_key)}"
     host        = "${aws_instance.backend.public_ip}"
   }
 
@@ -45,4 +46,3 @@ provisioner "local-exec" {
     EOT
 }
 }
-
